9570669176202758d743dcdf7941e21b6c5c4661
代小呆 <diauweb@live.com>
2021-12-22 15:43:32 +0000

    fix: remove file

--- src/main/java/io/github/nickid2018/dejava/ast/VarInitializer.java
+++
@@ -1,9 +0,0 @@
-package io.github.nickid2018.dejava.ast;
-
-// TODO
-class VarInitializer implements INode {
-    @Override
-    public String toSource(FormatControl fc) {
-        return "VarInitializer";
-    }
-}
