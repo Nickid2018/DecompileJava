da4fba4abbd2cecb4b8aac3892a312547da9460e
代小呆 <diauweb@live.com>
2021-12-24 13:43:54 +0000

    test: fix test

--- src/test/java/io/github/nickid2018/dejava/ast/FieldDeclTest.java
+++ src/test/java/io/github/nickid2018/dejava/ast/FieldDeclTest.java
@@ -2,6 +2,7 @@ package io.github.nickid2018.dejava.ast;
 
 import io.github.nickid2018.dejava.api.*;
 import io.github.nickid2018.dejava.ast.expr.IExpression;
+import io.github.nickid2018.dejava.ast.expr.literal.Literal;
 
 import org.junit.jupiter.api.*;
 
@@ -17,9 +18,9 @@ class FieldDeclTest {
             .addModifiers(Modifiers.PUBLIC).toSource();
         assertEquals("public int field0", result);
 
-        var result2 = new FieldDecl(Typename.of("int"),
-            Map.of("field1", new IExpression(){},
-                   "field2", new IExpression(){}))
+        var result2 = new FieldDecl(Typename.of("Integer"),
+            Map.of("field1", Literal.ofNull(),
+                   "field2", Literal.of(114514)))
             .addModifiers(Modifiers.PUBLIC).toSource();
 
         System.out.println(result);
